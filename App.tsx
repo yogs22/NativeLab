522013af490fe4e060dcbf9cc6cb03d38ce8df47
Yogi Prasetyawan <yogi@luwjistik.com>
2023-01-20 17:25:42 +0700

    Basic styling

--- App.tsx
+++ App.tsx
@@ -1,7 +1,72 @@
 import React, {Component} from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
+import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
+import mlimage from './pp-ml.jpg';
 
 const App = () => {
+  return <StylingComponent />;
+};
+
+const StylingComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: 'cyan',
+          borderWidth: 5,
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+      <View style={{padding: 12, backgroundColor: '#f2f2f2', width: 212}}>
+        <Image
+          source={mlimage}
+          style={{
+            width: 188,
+            height: 107,
+            borderRadius: 8,
+          }}
+        />
+        <Text
+          style={{
+            color: 'black',
+            fontSize: 14,
+            fontWeight: 'bold',
+            marginTop: 14,
+          }}>
+          Diamond Mobile Legends
+        </Text>
+        <Text style={{color: 'orange', marginTop: 10, marginBottom: 10}}>
+          Rp 100.000
+        </Text>
+        <Text>Moonton</Text>
+        <View
+          style={{
+            backgroundColor: 'crimson',
+            padding: 10,
+            borderRadius: 50,
+            marginVertical: 10,
+          }}>
+          <Text style={{color: '#fff', textAlign: 'center'}}>Beli Sekarang</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'green',
+    marginLeft: 20,
+    marginTop: 40,
+  },
+});
+
+const SampleComponent = () => {
   return (
     <View>
       <View style={{width: 80, height: 80, backgroundColor: 'cyan'}} />
